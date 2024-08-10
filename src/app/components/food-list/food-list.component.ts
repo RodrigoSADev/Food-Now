6144306e3010ec7ca17f53aeacdfc1ebda71a08e
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-08-10 16:27:28 -0300

    feat: add food-list component

---
+++ src/app/components/food-list/food-list.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FoodData, FoodItem } from 'src/app/interfaces/food.interface';
+import { FoodService } from 'src/app/services/food.service';
+
+@Component({
+  selector: 'app-food-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './food-list.component.html',
+  styleUrl: './food-list.component.scss',
+})
+export class FoodListComponent implements OnInit {
+  currentFoods: FoodItem[] = [];
+  allFoods: FoodData | undefined;
+  isLoading = true;
+  currentFoodType: string | undefined;
+
+  actRoute = inject(ActivatedRoute);
+  foodService = inject(FoodService);
+
+  ngOnInit(): void {
+    this.foodService.getFoods().subscribe({
+      next: (data: FoodData) => {
+        this.allFoods = data;
+        this.actRoute.url.subscribe((urlSegment) => {
+          this.currentFoodType = urlSegment[0].path;
+          const foodType = urlSegment[0].path as keyof FoodData;
+          this.currentFoods = this.allFoods
+            ? this.allFoods[foodType] || []
+            : [];
+        });
+      },
+      error: (error) => {
+        console.error('Erro ao carregar os dados:', error);
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+}
