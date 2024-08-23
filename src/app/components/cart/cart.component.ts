e330d8fb1fc134e988124f30256eb15804123931
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-08-22 22:18:23 -0300

    feat: add swal toast to cart actions

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, Signal } from '@angular/core';
 import { Cart } from 'src/app/interfaces/cart.interface';
 import { FoodItem } from 'src/app/interfaces/food.interface';
 import { CartService } from 'src/app/services/cart.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart',
@@ -15,15 +16,48 @@ export class CartComponent {
   cartService = inject(CartService);
   cart$: Signal<Cart> = this.cartService.cart;
 
-  addToCart(food: FoodItem): void {
+  addItem(food: FoodItem): void {
     this.cartService.addToCart(food);
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      icon: 'success',
+      title: 'Adicionado ao carrinho!',
+      text: `Item adicionado com sucesso!`,
+      showConfirmButton: false,
+      showCloseButton: true,
+      timer: 3000,
+      timerProgressBar: true,
+    });
   }
 
   removeItem(productId: number): void {
     this.cartService.removeFromCart(productId);
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      icon: 'success',
+      title: 'Removido do carrinho!',
+      text: `Item removido com sucesso!`,
+      showConfirmButton: false,
+      showCloseButton: true,
+      timer: 3000,
+      timerProgressBar: true,
+    });
   }
 
   clearCart(): void {
     this.cartService.clearCart();
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      icon: 'success',
+      title: 'Carrinho limpo!',
+      text: 'Todos os itens foram removidos do carrinho.',
+      showConfirmButton: false,
+      showCloseButton: true,
+      timer: 3000,
+      timerProgressBar: true,
+    });
   }
 }
