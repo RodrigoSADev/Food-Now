968bb91515569a01dc444d70fd43dd4abdb97f74
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-08-23 17:30:37 -0300

    feat: add address form

---
+++ src/app/components/address-form/address-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-address-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './address-form.component.html',
+  styleUrl: './address-form.component.scss',
+})
+export class AddressFormComponent {}
