1634c77985e40cf5899475599380d2529947244a
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-08-23 23:41:29 -0300

    feat: added form builder

--- src/app/components/payment-form/payment-form.component.ts
+++ src/app/components/payment-form/payment-form.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-payment-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './payment-form.component.html',
   styleUrl: './payment-form.component.scss',
 })
-export class PaymentFormComponent {}
+export class PaymentFormComponent {
+  formBuild = inject(FormBuilder);
+
+  paymentForm = this.formBuild.group({
+    paymentMethod: [null as string | null, Validators.required],
+  });
+
+  onSubmit() {
+    if (this.paymentForm.valid) {
+      const selectedPayment = this.paymentForm.value.paymentMethod;
+      console.log('Forma de pagamento selecionada:', selectedPayment);
+      // Adicione a lógica para enviar os dados do formulário
+    } else {
+      console.log('Por favor, selecione uma forma de pagamento.');
+    }
+  }
+}
