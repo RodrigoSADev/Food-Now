0be4f995db36c21ad891b9918f5066625439e48e
Rodrigo Silva de Almeida <rodrigosalmeida.pessoal@gmail.com>
2024-09-10 18:51:00 -0300

    feat: add position fixed to footer in determined routes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isFooterBottom = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url;
+        if (currentUrl === '/confirm-order' || currentUrl === '/checkout') {
+          console.log('true');
+          this.isFooterBottom = true;
+        } else {
+          console.log('false');
+          this.isFooterBottom = false;
+        }
+      }
+    });
+  }
+}
