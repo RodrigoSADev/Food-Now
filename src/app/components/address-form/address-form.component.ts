502b9f91ae107ad23229ae7bd5b6c29dd6884b81
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-24 19:55:54 -0300

    feat: add address form component for user address input and management

---
+++ src/app/components/address-form/address-form.component.ts
@@ -0,0 +1,122 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ICep } from '../../interfaces/cep.interface';
+import { CepService } from '../../services/cep.service';
+import { CheckoutService } from '../../services/checkout.service';
+
+@Component({
+  selector: 'app-address-form',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './address-form.component.html',
+  styleUrl: './address-form.component.scss',
+})
+export class AddressFormComponent {
+  formBuild = inject(FormBuilder);
+  cepService = inject(CepService);
+  checkoutService = inject(CheckoutService);
+
+  addressForm: FormGroup = this.formBuild.group({
+    cep: ['', [Validators.required, Validators.minLength(8)]],
+    city: ['', [Validators.required, Validators.minLength(2)]],
+    neighborhood: ['', [Validators.required, Validators.minLength(5)]],
+    street: ['', [Validators.required, Validators.minLength(5)]],
+    number: ['', [Validators.required, Validators.minLength(1)]],
+    complement: [''],
+    saveAddress: [false],
+  });
+
+  ngOnInit(): void {
+    // Carregar endereço salvo do localStorage, se existir
+    const savedAddress = localStorage.getItem('savedAddress');
+    if (savedAddress) {
+      const addressData = JSON.parse(savedAddress);
+      this.addressForm.patchValue(addressData);
+      this.addressForm.get('saveAddress')?.setValue(true);
+      this.checkoutService.address.set(addressData);
+    }
+
+    // Ouvir mudanças no checkbox
+    this.addressForm.get('saveAddress')?.valueChanges.subscribe((isChecked) => {
+      if (isChecked) {
+        this.saveAddressToLocalStorage();
+        this.enableAutoSave();
+      } else {
+        localStorage.removeItem('savedAddress');
+      }
+    });
+
+    // Se o checkbox já estava marcado, ativar o autosave
+    if (this.addressForm.get('saveAddress')?.value) {
+      this.enableAutoSave();
+    } else {
+      this.checkoutService.setAddress({
+        cep: '',
+        city: '',
+        neighborhood: '',
+        street: '',
+        number: '',
+        complement: '',
+      });
+    }
+  }
+
+  enableAutoSave(): void {
+    // Salvar o endereço automaticamente quando houver mudanças no formulário
+    this.addressForm.valueChanges.subscribe(() => {
+      if (this.addressForm.get('saveAddress')?.value) {
+        this.saveAddressToLocalStorage();
+      }
+    });
+  }
+
+  saveAddressToLocalStorage(): void {
+    // Salvar o endereço no localStorage para ser carregado posteriormente
+    const addressData = {
+      cep: this.addressForm.get('cep')?.value,
+      city: this.addressForm.get('city')?.value,
+      neighborhood: this.addressForm.get('neighborhood')?.value,
+      street: this.addressForm.get('street')?.value,
+      number: this.addressForm.get('number')?.value,
+      complement: this.addressForm.get('complement')?.value,
+    };
+
+    localStorage.setItem('savedAddress', JSON.stringify(addressData));
+  }
+
+  onBlurCep() {
+    // Remover caracteres não numéricos do Cep e verificar se o Cep está preenchido
+    let cep = this.addressForm.get('cep')?.value;
+    // Verificar se o Cep tem somente dígitos
+    cep = cep?.replace(/\D/g, '');
+    if (cep !== null && cep !== undefined && cep !== '') {
+      // Regex para validar o Cep
+      const validaCep = /^[0-9]{8}$/;
+      if (validaCep.test(cep)) {
+        // Buscar dados do Cep usando a API
+        this.cepService.searchCep(cep).subscribe((cep: ICep) => {
+          this.addressForm.patchValue({
+            city: cep.localidade,
+            neighborhood: cep.bairro,
+            street: cep.logradouro,
+          });
+        });
+      }
+    }
+  }
+
+  onUpdateAddress(): void {
+    // Atualiza o signal no serviço sempre que houver mudança
+    this.checkoutService.setAddress(this.addressForm.value);
+    if (this.addressForm.valid) {
+      this.checkoutService.showAddressErrorMessage.set(false);
+    } else {
+      this.checkoutService.showAddressErrorMessage.set(true);
+    }
+  }
+}
