f158808753dcc2e1b800612bce233f9ecb2e5c9a
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-24 19:28:15 -0300

    feat: add payment form component for selecting payment methods

---
+++ src/app/components/payment-form/payment-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CheckoutService } from '../../services/checkout.service';
+
+@Component({
+  selector: 'app-payment-form',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './payment-form.component.html',
+  styleUrl: './payment-form.component.scss',
+})
+export class PaymentFormComponent {
+  formBuild = inject(FormBuilder);
+  checkoutService = inject(CheckoutService);
+
+  paymentForm = this.formBuild.group({
+    paymentMethod: ['', [Validators.required]],
+  });
+
+  onSelectPaymentMethod(method: string) {
+    // Seleciona o método de pagamento e dispara um evento para o service
+    if (this.paymentForm.valid) {
+      this.checkoutService.setPaymentMethod(method);
+      this.checkoutService.showPaymentErrorMessage.set(false); // Esconde a mensagem de erro quando um método é selecionado
+    }
+  }
+}
