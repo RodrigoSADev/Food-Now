ca73fbba723387f34fe23af6db9c8303beac0b6c
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-24 19:55:40 -0300

    feat: add address management to CheckoutService with IAddress interface

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal } from '@angular/core';
+import { IAddress } from '../interfaces/address.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,6 +8,16 @@ export class CheckoutService {
   paymentMethod = signal('');
   showPaymentErrorMessage = signal(false);
 
+  address = signal<IAddress>({
+    cep: '',
+    city: '',
+    neighborhood: '',
+    street: '',
+    number: '',
+    complement: '',
+  });
+  showAddressErrorMessage = signal(false);
+
   // Métodos relacionados ao pagamento
   getPaymentMethod(): string {
     return this.paymentMethod();
@@ -14,5 +25,14 @@ export class CheckoutService {
 
   setPaymentMethod(method: string): void {
     this.paymentMethod.set(method);
+  }
+
+  // Métodos relacionados ao endereço
+  getAddress(): IAddress {
+    return this.address();
+  }
+
+  setAddress(address: IAddress): void {
+    this.address.set(address);
   }
 }
