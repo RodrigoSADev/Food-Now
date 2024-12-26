a5d6bdf1fccc8de3d211a8178732bdb740da9a5e
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-26 15:47:54 -0300

    test: enhance CheckoutService tests with payment method and address validation

--- src/app/services/checkout.service.spec.ts
+++ src/app/services/checkout.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { IAddress } from '../interfaces/address.interface';
 import { CheckoutService } from './checkout.service';
 
 describe('CheckoutService', () => {
   let service: CheckoutService;
+  const mockAddress: IAddress = {
+    cep: '12345678',
+    city: 'City',
+    neighborhood: 'Neighborhood',
+    street: 'Street',
+    number: '123',
+    complement: 'Apt 1',
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +20,41 @@ describe('CheckoutService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set and get payment method', () => {
+    service.setPaymentMethod('credit_card');
+    expect(service.paymentMethod()).toBe('credit_card');
+  });
+
+  it('should validate payment method', () => {
+    service.setPaymentMethod('credit_card');
+    expect(service.validatePayment()).toBe(true);
+    service.setPaymentMethod('');
+    expect(service.validatePayment()).toBe(false);
+  });
+
+  it('should set and get saved payment method', () => {
+    service.setSavedPaymentMethod('credit_card');
+    expect(service.getSavedPaymentMethod()).toBe('credit_card');
+  });
+
+  it('should set and get address', () => {
+    service.setAddress(mockAddress);
+    expect(service.getAddress()).toEqual(mockAddress);
+  });
+
+  it('should validate address', () => {
+    service.setAddress(mockAddress);
+    expect(service.validateAddress()).toBe(true);
+    service.setAddress({
+      cep: '',
+      city: '',
+      neighborhood: '',
+      street: '',
+      number: '',
+      complement: '',
+    });
+    expect(service.validateAddress()).toBe(false);
   });
 });
