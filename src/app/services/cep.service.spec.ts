43001fa5f317fae273677c1ed09baf38b6ff02dc
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-26 14:26:28 -0300

    test: enhance CepService tests with HTTP client integration and mock data

--- src/app/services/cep.service.spec.ts
+++ src/app/services/cep.service.spec.ts
@@ -1,18 +1,47 @@
 import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ICep } from '../interfaces/cep.interface';
 import { CepService } from './cep.service';
 
 describe('CepService', () => {
   let service: CepService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
+      imports: [],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
+
     service = TestBed.inject(CepService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch CEP data', () => {
+    const mockCepData: ICep = {
+      logradouro: 'Praça da Sé',
+      bairro: 'Sé',
+      localidade: 'São Paulo',
+    };
+
+    service.searchCep('01001000').subscribe((data) => {
+      expect(data).toEqual(mockCepData);
+    });
+
+    const req = httpMock.expectOne('https://viacep.com.br/ws/01001000/json/');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockCepData);
   });
 });
